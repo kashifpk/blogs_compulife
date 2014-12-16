5019d998c104b77d18ebf8c300c216943138d75a
Kashif Iftikhar <kashif@compulife.com.pk>
2014-12-17 03:42:39 +0500

    Initial project template for blogs.compulife.com.pk

---
+++ blogs_compulife/apps/__init__.py
@@ -0,0 +1,13 @@
+# This list contains all the enabled apps for the project, when you add a new app, add it to this list
+# to make it accessible by the project.
+
+import importlib
+
+PROJECT_NAME = 'blogs_compulife'
+project_package = importlib.import_module("blogs_compulife")
+
+#from . import app1, app2
+#enabled_apps = [app1, app2]
+
+enabled_apps = []
+
