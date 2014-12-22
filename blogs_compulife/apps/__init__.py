66b3bb948f55074e9779cacd84f14cf7e8ccc9be
Kashif Iftikhar <kashif@compulife.com.pk>
2014-12-22 21:49:06 +0500

    Moved has_app to apps package

--- blogs_compulife/apps/__init__.py
+++ blogs_compulife/apps/__init__.py
@@ -6,5 +6,19 @@ import importlib
 PROJECT_NAME = 'blogs_compulife'
 project_package = importlib.import_module("blogs_compulife")
 
+
+def has_app(app_name):
+    "Checks if a given app is available in project's enabled apps"
+
+    for app in enabled_apps:
+        if app_name == app.__name__.split('.')[-1]:
+            return True
+
+    return False
+
+
 from . import blog, visit_counter, geoip
 enabled_apps = [blog, visit_counter, geoip]
+
+
+
